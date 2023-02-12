c4db1a298bffba8b71b6b04e16b51c564fe26c2f
samruddhithakur25 <125183029+samruddhithakur25@users.noreply.github.com>
2023-02-12 14:26:52 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,20 @@
+let strings="";
+let buttons=document.querySelectorAll('button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click' ,(e)=>{
+        if(e.target.innerHTML == "="){
+            string = eval(string);
+            document.querySelector(' .result') .value = string;
+        }
+        else if(e.target.innerHTML =="Clear"){
+            string = "";
+            document .querySelector(' .result').value = strings;
+        }
+        else{
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector(' .result').value = string;
+        }
+
+    })
+})
